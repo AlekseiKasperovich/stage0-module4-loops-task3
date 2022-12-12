66859533bb172f9c4da79283537312855274b99a
AlekseiKasperovich <AleksKasperovich@yandex.by>
2022-12-12 16:24:56 +0300

    commit

--- src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
@@ -1,6 +1,9 @@
 package school.mjc.stage0.loops.task3;
 
 public class AlphabetPrinter {
+
     public void printAlphabet() {
+        for (char i = 'A'; i <= 'Z'; i++)
+            System.out.println(i);
     }
 }
